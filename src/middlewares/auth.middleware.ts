d72be29f9f53ed7fc9dada41f8bdebb7d6d9b23a
callanbrombacher <callan@sitebydesign.co.za>
2024-06-20 14:51:20 +0200

    Created auth middleware:
    - added a logout route to destroy session
    - added a auth middleware that checks for a valid session
    - added the middleware to all the necessary routes

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,8 @@
+import { NextFunction, Request, Response } from 'express';
+export const AuthMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  if (req.session && req.session.userId) {
+    next();
+  } else {
+    res.status(401).json({ error: 'Unauthorized' });
+  }
+};
--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -3,7 +3,8 @@ import { AuthController } from '@controllers/auth.controller';
 import { CreateUserDto, DeleteUserDto, LoginUserDto } from '@dtos/users.dto';
 import { Routes } from '@interfaces/routes.interface';
 import { ValidationMiddleware } from '@middlewares/validation.middleware';
-import {DeleteDeviceDto} from "@dtos/device.dto";
+import { DeleteDeviceDto } from '@dtos/device.dto';
+import { AuthMiddleware } from '@middlewares/auth.middleware';
 
 export class AuthRoute implements Routes {
   public router = Router();
@@ -16,7 +17,8 @@ export class AuthRoute implements Routes {
   private initializeRoutes() {
     this.router.post('/auth/signup', ValidationMiddleware(CreateUserDto), this.auth.signUp);
     this.router.post('/auth/login', ValidationMiddleware(LoginUserDto), this.auth.logIn);
-    this.router.post('/auth/deRegisterUser', ValidationMiddleware(DeleteUserDto), this.auth.deRegisterUser);
-    this.router.post('/auth/deRegisterDevice', ValidationMiddleware(DeleteDeviceDto), this.auth.deregisterDevice);
+    this.router.post('/auth/deRegisterUser', AuthMiddleware, ValidationMiddleware(DeleteUserDto), this.auth.deRegisterUser);
+    this.router.post('/auth/deRegisterDevice', AuthMiddleware, ValidationMiddleware(DeleteDeviceDto), this.auth.deregisterDevice);
+    this.router.post('/auth/logout', AuthMiddleware, this.auth.logout);
   }
 }
