92633124003538a7cc4486ab50517c96101b68cb
Bongisipho <casseteinc@gmail.com>
2024-11-21 16:37:15 +0200

    More progress made.
    - Fixed and tested the create alerts endpoint
    
    - started working on getAlertByUserId

--- src/services/alert.service.ts
+++ src/services/alert.service.ts
@@ -2,14 +2,15 @@ import { Service } from 'typedi';
 import pg from '@database';
 import { Group } from '@interfaces/group.interface';
 import { Alert } from '@interfaces/alert.interface';
+import {CreateAlertDto} from "@dtos/alert.dto";
 
 @Service()
 export class AlertService {
-  public createAlert = async (alert: Alert): Promise<Group | boolean | NodeJS.ErrnoException> => {
-    const { triggering_device_id, location, push_notification_id, id } = alert;
-    const sql = `INSERT INTO groups (triggering_device_id, location, push_notification_id, id) VALUES ( $1, $2, $3, $4) RETURNING *`;
+  public createAlert = async (alert: CreateAlertDto): Promise<Group | boolean | NodeJS.ErrnoException> => {
+    const { device_uuid, location } = alert;
+    const sql = `INSERT INTO alerts (triggering_device_id, location) VALUES ( (SELECT id FROM devices WHERE device_uuid=$1), $2) RETURNING *;`;
     return await pg
-      .query(sql, [triggering_device_id, location, push_notification_id, id])
+      .query(sql, [device_uuid, location])
       .then(result => {
         if (result.rowCount > 0) {
           return result.rows[0];
@@ -52,6 +53,25 @@ export class AlertService {
   }
 
   async getAlertByUserId(userId: number, groupId: number):Promise<Group | boolean | NodeJS.ErrnoException> {
+    const sql = `SELECT alerts.* FROM alerts
+               JOIN devices ON  device.id = alerts.trigerring_device_id
+               JOIN users ON  users.id = devices.user_id
+               WHERE devices.user_id = $1 and groups.id = $2 ;`;
+
+
+    return await pg
+      .query(sql, [userId, groupId])
+      .then(result => {
+        if (result.rowCount > 0) {
+          return result.rows[0];
+        } else {
+          return false;
+        }
+      })
+      .catch(err => err);
+  }
+
+  async getAlertByGroupId(groupId: number):Promise<Group | boolean | NodeJS.ErrnoException> {
     // const sql = 'SELECT\n' +
     //   '  alerts.id AS alert_id,\n' +
     //   '  alerts.time AS alert_time,\n' +
@@ -62,11 +82,6 @@ export class AlertService {
     //   '  LEFT JOIN push_notifications ON alerts.push_notifications_id = push_notifications.id;';
     //
 
-    console.log("Dynamic URL works")
-    return false;
-  }
-
-  async getAlertByGroupId(groupId: number):Promise<Group | boolean | NodeJS.ErrnoException> {
     console.log("Dynamic URL works")
     return false;
   }
