3e87ffd666716592b1cf2152b9b1d7592481c648
Bongisipho <casseteinc@gmail.com>
2024-11-21 04:17:40 +0200

    Fixed CRUD for alerts in alert.service.ts

--- src/services/alert.service.ts
+++ src/services/alert.service.ts
@@ -1,14 +1,15 @@
 import { Service } from 'typedi';
 import pg from '@database';
 import { Group } from '@interfaces/group.interface';
+import { Alert } from '@interfaces/alert.interface';
 
 @Service()
 export class AlertService {
   public createAlert = async (alert: Alert): Promise<Group | boolean | NodeJS.ErrnoException> => {
-    const { name, created_by_user_id, identification_string } = group;
-    const sql = `INSERT INTO groups (name, created_by_user_id, identification_string) VALUES ( $1, $2, $3) RETURNING *`;
+    const { triggering_device_id, location, push_notification_id, id } = alert;
+    const sql = `INSERT INTO groups (triggering_device_id, location, push_notification_id, id) VALUES ( $1, $2, $3, $4) RETURNING *`;
     return await pg
-      .query(sql, [name, created_by_user_id, identification_string])
+      .query(sql, [triggering_device_id, location, push_notification_id, id])
       .then(result => {
         if (result.rowCount > 0) {
           return result.rows[0];
@@ -19,12 +20,12 @@ export class AlertService {
       .catch(error => error);
   };
 
-  public updateAlert = async (group: Group): Promise<Group | boolean | NodeJS.ErrnoException> => {
-    const { id, name, created_by_user_id } = group;
+  public updateAlert = async (alert: Alert): Promise<Group | boolean | NodeJS.ErrnoException> => {
+    const { triggering_device_id, location, push_notification_id, id } = alert;
     const sql = `
-      UPDATE groups SET name = $1, created_by_user_id = $2 WHERE id = $3 RETURNING *;`;
+      UPDATE groups SET triggering_device_id = $1, location = $2, push_notification_id=$3 WHERE id = $4 RETURNING *;`;
     return await pg
-      .query(sql, [name, created_by_user_id, id])
+      .query(sql, [triggering_device_id, location, push_notification_id, id])
       .then(result => {
         if (result.rowCount > 0) {
           return result.rows[0];
@@ -35,8 +36,8 @@ export class AlertService {
       .catch(error => error);
   };
 
-  async deleteAlert(group: Group): Promise<Group | boolean | NodeJS.ErrnoException> {
-    const { id } = group;
+  async deleteAlert(alert: Alert): Promise<Group | boolean | NodeJS.ErrnoException> {
+    const { id } = alert;
     const sql = `Delete FROM groups where id = $1 RETURNING *;`;
     return await pg
       .query(sql, [id])
