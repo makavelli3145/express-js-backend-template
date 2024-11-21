9ac384f2eee9a9485e6b83db954c03d6e41d2b4b
callanbrombacher <callan@sitebydesign.co.za>
2024-11-21 13:37:44 +0200

    Fixed networ issues

--- src/controllers/alert.controller.ts
+++ src/controllers/alert.controller.ts
@@ -4,128 +4,126 @@ import { Alert } from '@interfaces/alert.interface';
 import { AlertService } from '@services/alert.service';
 
 export class AlertController {
-  public alertService = Container.get(AlertService);
-  public createAlert = (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const alert: Alert = req.body;
-      this.alertService.createAlert(alert).then(result => {
-        if (result) {
-          res.status(200).json(result);
-        } else {
-          res.status(500).send('an alert could not be created at this time');
+    public alertService = Container.get(AlertService);
+    public createAlert = (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const alert: Alert = req.body;
+            this.alertService.createAlert(alert).then(result => {
+                if (result) {
+                    res.status(200).json(result);
+                } else {
+                    res.status(500).send('an alert could not be created at this time');
+                }
+            });
+        } catch (error) {
+            next(error);
         }
-      });
-    } catch (error) {
-      next(error);
-    }
-  };
+    };
 
-  public deleteAlert = (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const alert: Alert = req.body;
-      this.alertService.deleteAlert(alert).then(result => {
-        if (result) {
-          res.status(200).json(result);
-        } else {
-          res.status(500).send('There was an error with deleting an alert');
+    public deleteAlert = (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const alert: Alert = req.body;
+            this.alertService.deleteAlert(alert).then(result => {
+                if (result) {
+                    res.status(200).json(result);
+                } else {
+                    res.status(500).send('There was an error with deleting an alert');
+                }
+            });
+        } catch (error) {
+            next(error);
         }
-      });
-    } catch (error) {
-      next(error);
-    }
-  };
+    };
 
-  public updateAlert = (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const alert: Alert = req.body;
-      this.alertService.updateAlert(alert).then(result => {
-        if (result) {
-          res.status(200).json(result);
-        } else {
-          res.status(500).send('an alert could not be updated at this time');
+    public updateAlert = (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const alert: Alert = req.body;
+            this.alertService.updateAlert(alert).then(result => {
+                if (result) {
+                    res.status(200).json(result);
+                } else {
+                    res.status(500).send('an alert could not be updated at this time');
+                }
+            });
+        } catch (error) {
+            next(error);
         }
-      });
-    } catch (error) {
-      next(error);
-    }
-  };
-  public getAlertByUserId = (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const userId = Number(req.query.id);
-      this.alertService.getAlertByUserId(userId).then(result => {
-        if (result) {
-          res.status(200).json(result);
-        } else {
-          res.status(500).send('server unable to get alerts by groupId');
-        }
-      });
-    } catch (error) {
-      next(error);
-    }
-  };
-
-  const
-  checkAlertParams = (params: any) => {
-    const validParams = ["all", "userId", "groupId"];
-    return validParams.includes(params);
-  };
-  public getAlertByUserAndGroupId = (req: Request, res: Response, next: NextFunction) => {
-
-    const isValidParams = this.checkAlertParams(req.params.filterBy);
-    console.log("isValidParams: ", isValidParams)
-
-    if (isValidParams) {
-      const urlParams = req.params.filterBy
-      switch (urlParams) {
-        default:
-          res.status(500).send("There is an issue with getting alerts")
-
-        case "userId":
-          try {
-            const userId = Number(req.params.userId);
+    };
+    public getAlertByUserId = (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const userId = Number(req.query.id);
             this.alertService.getAlertByUserId(userId).then(result => {
-              if (result) {
-                res.status(200).json(result);
-              } else {
-                res.status(500).send('server unable to get alerts by userId and groupId');
-              }
+                if (result) {
+                    res.status(200).json(result);
+                } else {
+                    res.status(500).send('server unable to get alerts by groupId');
+                }
             });
-          }catch(error){
-              next(error);
-          }
-
-        case "groupId":
-          try {
-            const groupId = Number(req.params.groupId);
-
-            this.alertService.getAlertByGroupId(groupId).then(result => {
-              if (result) {
-                res.status(200).json(result);
-              } else {
-                res.status(500).send('server unable to get alerts by userId and groupId');
-              }
-            });
-          } catch(error){
-              next(error);
-          }
-        case "all":
-          try {
-            const userId = Number(req.params.userId);
-            const groupId = Number(req.params.groupId);
-
-            this.alertService.getAlertByUserAndGroupId(userId, groupId).then(result => {
-              if (result) {
-                res.status(200).json(result);
-              } else {
-                res.status(500).send('server unable to get alerts by userId and groupId');
-              }
-            });
-          } catch(error){
-              next(error);
-          }
+        } catch (error) {
+            next(error);
         }
-      }else {
-      res.status(500).send("Invalid url parameters")
-    }
-  }
+    };
+
+    const;
+    checkAlertParams = (params: any) => {
+        const validParams = ['all', 'userId', 'groupId'];
+        return validParams.includes(params);
+    };
+    public getAlertByUserAndGroupId = (req: Request, res: Response, next: NextFunction) => {
+        const filterBy = req.params.filterBy;
+        switch (filterBy) {
+            case 'userId':
+                try {
+                    const groupId = parseInt(req.params.groupId);
+                    const userId = parseInt(req.params.userId);
+                    if (userId && groupId) {
+                        this.alertService.getAlertByUserId(userId).then(result => {
+                            if (result) {
+                                res.status(200).json(result);
+                            } else {
+                                res.status(500).send('server unable to get alerts by userId and groupId');
+                            }
+                        });
+                    } else {
+                        res.status(501).send('invalid request, query string is missing group ID value or user ID value');
+                    }
+                } catch (error) {
+                    next(error);
+                }
+                break;
+            case 'groupId':
+                try {
+                    const groupId = parseInt(req.params.groupId);
+                    if (groupId) {
+                        this.alertService.getAlertByGroupId(groupId).then(result => {
+                            if (result) {
+                                res.status(200).json(result);
+                            } else {
+                                res.status(500).send('server unable to get alerts by userId and groupId');
+                            }
+                        });
+                    } else {
+                        res.status(501).send('invalid request, query string is missing group ID value');
+                    }
+                } catch (error) {
+                    next(error);
+                }
+                break;
+            case 'all':
+                try {
+                    this.alertService.getAllAlerts().then(result => {
+                        if (result) {
+                            res.status(200).json(result);
+                        } else {
+                            res.status(500).send('server unable to get alerts by userId and groupId');
+                        }
+                    });
+                } catch (error) {
+                    next(error);
+                }
+                break;
+            default:
+                res.status(500).send('There is an issue with getting alerts');
+        }
+    };
 }
