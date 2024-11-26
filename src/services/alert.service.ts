48a71300f6b45faa3f16e474f54442ed01717be4
Bongisipho <casseteinc@gmail.com>
2024-11-26 09:16:39 +0200

    Removed unnecessary join

--- src/services/alert.service.ts
+++ src/services/alert.service.ts
@@ -70,11 +70,10 @@ export class AlertService {
 
   async getAlertByGroupId(groupId: number):Promise<Group | boolean | NodeJS.ErrnoException> {
     const sql = `SELECT alerts.* FROM alerts
-                  JOIN devices ON devices.id = alerts.triggering_device_id
-                  JOIN users ON users.id = devices.user_id
-                  JOIN users_groups ON users_groups.user_id = users.id
-                  JOIN groups ON groups.id = users_groups.group_id
-                 WHERE groups.id = $1;`;
+      JOIN devices ON devices.id = alerts.triggering_device_id
+      JOIN users ON users.id = devices.user_id
+      JOIN users_groups ON users_groups.user_id = users.id
+      WHERE users_groups.group_id = $1;`;
 
 
     return await pg
